e80fa727f2c953bb91b86adb21e9c1b2726cadc9
Matvey Smorodin <mat.smooooder@gmail.com>
2022-09-14 14:28:36 +0300

    init commit

---
+++ src/main/java/ClickCounter.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ClickCounter {
+    private JPanel panel;
+    private JFrame frame;
+    private JButton button;
+    private JLabel label;
+    private int counter;
+
+    private ClickCounter() {
+        frame = new JFrame();
+        panel = new JPanel();
+        button = new JButton("click me!");
+        label = new JLabel(getLabelText(counter));
+        setup();
+    }
+
+    public static void main(String[] args) {
+        new ClickCounter();
+    }
+
+    private void setup() {
+        button.addActionListener((actionEvent) -> {
+            counter++;
+            label.setText(getLabelText(counter));
+        });
+        panel.add(button);
+        panel.add(label);
+        frame.add(panel);
+        frame.setTitle("click-counter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        frame.setLayout(new GridLayout());
+        frame.setVisible(true);
+    }
+
+    private String getLabelText(int counter) {
+        return String.format("Number of clicks: %d", counter);
+    }
+}
